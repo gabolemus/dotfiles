6526bce1d080e5852527fa94587ae1801ed35842
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2025-03-08 15:14:44 -0600

    Refactor plugin setup + add key bindings

--- config/nvim/lua/plugins/noice.lua
+++ config/nvim/lua/plugins/noice.lua
@@ -1,15 +1,23 @@
 return {
     "folke/noice.nvim",
-    event = "VeryLazy",
+    enabled = true,
     dependencies = {
         "MunifTanjim/nui.nvim",
         "rcarriga/nvim-notify",
     },
+    event = "VeryLazy",
     config = true,
+    opts = {
+        lsp = {
+            hover = {
+                enabled = false,
+            },
+        },
+    },
     keys = {
         { "<leader>nd", "<cmd>Noice dismiss<cr>", desc = "Dismiss notification pop-ups" },
-        { "<leader>nl", "<cmd>Noice last<cr>", desc = "Show the last notification pop-up" },
-        { "<leader>ne", "<cmd>Noice errors<cr>", desc = "Show errors in a split" },
-        { "<leader>nt", "<cmd>Noice telescope<cr>", desc = "Opens noice notifications in a Telescope window" },
+        { "<leader>nl", "<cmd>Noice last<cr>", desc = "Show the last notification" },
+        { "<leader>ne", "<cmd>Noice errors<cr>", desc = "Show the errors in a split" },
+        { "<leader>nt", "<cmd>Noice telescope<cr>", desc = "Opens the message history in Telescope" },
     },
 }
