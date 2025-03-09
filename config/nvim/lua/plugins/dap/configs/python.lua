fa176ed6f1e2ae6f40083ed58544e9c24eac870d
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2025-03-09 14:40:56 -0600

    Dynamically get the path from Mason

--- config/nvim/lua/plugins/dap/configs/python.lua
+++ config/nvim/lua/plugins/dap/configs/python.lua
@@ -5,7 +5,7 @@ return {
         "mfussenegger/nvim-dap",
     },
     config = function()
-        local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
-        require("dap-python").setup(path)
+        local debugpy_path = require("mason-registry").get_package("debugpy"):get_install_path() .. "/venv/bin/python"
+        require("dap-python").setup(debugpy_path)
     end,
 }
