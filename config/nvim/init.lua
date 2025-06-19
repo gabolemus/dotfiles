1fa454efbe794720ac8e08e675a01ed008169c91
Gabriel Lemus <gabriel.lemusp@gmail.com>
2025-06-19 10:33:58 -0600

    Add initial config

---
+++ config/nvim/init.lua
@@ -0,0 +1,21 @@
+-- Line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Splits
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- Disable line wrap
+vim.opt.wrap = false
+
+-- Tabs
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+
+-- Clipboard
+vim.opt.clipboard = "unnamedplus" -- Sync the system clipboard with Neovim's clipboard
+
+-- Scroll
+vim.opt.scrolloff = 999 -- Keeps the cursor in the middle of the screen while scrolling
+
