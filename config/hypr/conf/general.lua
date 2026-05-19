7059d27993da23b3cd2f908a90a7399d25ae3038
Gabriel Lemus <49124491+gabolemus@users.noreply.github.com>
2026-05-18 19:31:48 -0600

    chore: remove commented-out code

--- config/hypr/conf/general.lua
+++ config/hypr/conf/general.lua
@@ -4,11 +4,6 @@ hl.config({
 		gaps_out = 20,
 		border_size = 2,
 
-		-- col = {
-		-- 	active_border = { colors = { "rgba(33ccffee)", "rgba(00ff99ee)" }, angle = 45 },
-		-- 	inactive_border = "rgba(595959aa)",
-		-- },
-
 		resize_on_border = true,
 
 		layout = "dwindle",
